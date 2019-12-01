98d779151a472315ab071bfc4dc65545a7a60a2f
Bojan Todorovic <bojankosta1989@gmail.com>
2019-12-01 14:29:19 +0100

    attribute names added

---
+++ src/main/java/kosta/programming/todolist/util/AttributeNames.java
@@ -0,0 +1,7 @@
+package kosta.programming.todolist.util;
+
+public class AttributeNames {
+    public static final String PRODUCT_LIST = "listProducts";
+    public static final String TODO_ITEM = "todoitem";
+    public static final String PATH_ID = "id";
+}
---
+++ src/main/java/kosta/programming/todolist/util/Views.java
@@ -0,0 +1,9 @@
+package kosta.programming.todolist.util;
+
+public class Views {
+    public static final String EDIT = "edit-todo";
+    public static final String NEW = "new-todo";
+    public static final String INDEX = "index";
+    public static final String REDIRECT = "redirect:/";
+
+}
